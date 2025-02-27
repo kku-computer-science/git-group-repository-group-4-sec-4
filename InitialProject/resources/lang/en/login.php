9acf7efadb9c74d922ba5c167597da00557eb84a
Samawann <samawan.k@kkumail.com>
2025-02-27 07:30:19 +0700

    Update login.php

--- InitialProject/resources/lang/en/login.php
+++ InitialProject/resources/lang/en/login.php
@@ -9,8 +9,4 @@ return [
     'forget_password' => '*** Forgot your password? Contact the system administrator.',
     'note_1' => 'For Username, use KKU-Mail to log in.',
     'note_2' => 'For students entering the system for the first time, log in with your student ID.',
-
-
-    
-
 ];
